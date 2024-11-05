0ffb77bcb6b8a48cfbc390d5a4e1197b2897d934
ExplodedRiot <alifananda66@gmail.com>
2024-11-05 23:46:18 +0700

    tugas-reactjs

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="outer-container">
+      <div className="App">
+        <h1>Hi, Saya Alif</h1>
+        <hr />
+        <div className="container">
+          <p>Saya dari Batch 36 sudah berhasil menginstal React</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
