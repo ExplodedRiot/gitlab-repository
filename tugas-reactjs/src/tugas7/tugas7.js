add59137d8f58a18865748845ff5ce6fc9463a19
ExplodedRiot <alifananda66@gmail.com>
2024-11-07 10:00:36 +0700

    tugas7?

---
+++ tugas-reactjs/src/tugas7/tugas7.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tugas6 from './tugas6/tugas6';
+import Tugas7 from './tugas7/tugas7';
+
+const App = () => {
+  return (
+    <>
+      <Tugas6 />
+      <Tugas7 name="Muhammad Alif Ananda" batch="36" email="alifananda66@gmail.com" />
+    </>
+  );
+}
+
+export default App;
